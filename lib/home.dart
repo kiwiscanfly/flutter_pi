b9a87fd4c35b31e76f0e1c65f82b5d42d250ea72
Rebecca Milne <rebecca@bex.codes>
2025-03-29 15:03:22 +1300

    Daemons and adding a button to the display

--- lib/home.dart
+++ lib/home.dart
@@ -1,17 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  String greeting = dotenv.env['GREETING'] ?? 'Hello, World!';
+
+  void changeGreeting() {
+    setState(() {
+      greeting = 'That tickles!!!';
+    });
+
+    // Change back to the original greeting after 2 seconds
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        greeting = dotenv.env['GREETING'] ?? 'Hello, World!';
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 155, 175, 192),
       body: Center(
-        child: Text(
-          dotenv.env['GREETING'] ?? 'Hello, World!',
-          style: TextStyle(color: Colors.white, fontSize: 84),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              greeting,
+              style: const TextStyle(color: Colors.white, fontSize: 84),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: changeGreeting,
+              style: ElevatedButton.styleFrom(
+                padding: const EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+              ),
+              child: const Text(
+                'Push Me!', 
+                style: TextStyle(fontSize: 24),
+              ),
+            ),
+          ],
         ),
       ),
     );
